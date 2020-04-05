d848cfb8167b1177c6c3f5e3c1e53166987c35d8
user74 <housechel@mail.ru>
2020-04-05 10:13:01 +0500

    polling = true, correcred out_message not message canal

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def start(message):
     send_chanel = f'<b>Бот статистики COVID-19.</b>\nВремя проверки - <b>{datetime.now()}</b>\n' \
                   f'Введите страну из списка (США, Испания, Италия, Германия, Китай, Франция, Иран, Англия, Турция, Росcия, Япония, Украина):'
     bot.send_message(message.chat.id, send_text, parse_mode='html')
-    bot.send_message('@covid19word', send_chanel, parse_mode='html')
+    # bot.send_message('@covid19word', send_chanel, parse_mode='html')
     log.info(f'Called bot.. name: {message.from_user.first_name}, command: /start')
 
 
@@ -44,8 +44,9 @@ def map(message):
     send_chanel = f'<b>Карта статистики COVID-19 в мире.</b>\nВремя проверки - <b>{datetime.now()}</b>\n' \
                   f'<a href="https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6">Word maps! Press.. me </a>'
     bot.send_message(message.chat.id, send_text, parse_mode='html')
-    bot.send_message('@covid19word', send_chanel, parse_mode='html')
+    # bot.send_message('@covid19word', send_chanel, parse_mode='html')
     log.info(f'Called bot.. name: {message.from_user.first_name}, command: /map')
+
 
 @bot.message_handler(content_types=['text'])
 def mess(message):
@@ -63,17 +64,17 @@ def mess(message):
         location = covid_19.getLocationByCountryCode('ES')
     elif get_message_bot == 'китай':
         location = covid_19.getLocationByCountryCode('CN')
-        out_message = f'<u>Данные не корректны!</u>\n<b> Заболевших на 03.04.2020 в Китае: 82 502</b>'
+        out_message = f'<u>Данные не корректны!</u>\n<b> Заболевших на 03.04.2020 в Китае: 82 574</b>'
         bot.send_message(message.chat.id, out_message, parse_mode='html')
-        bot.send_message('@covid19word', out_message, parse_mode='html')
+        # bot.send_message('@covid19word', out_message, parse_mode='html')
         out_message = ''
     elif get_message_bot == 'германия':
         location = covid_19.getLocationByCountryCode('DE')
     elif get_message_bot == 'франция':
         location = covid_19.getLocationByCountryCode('FR')
-        out_message = f'<u>Данные не корректны!</u>\n<b> Заболевших на 03.04.2020 в Франции: 59 929</b>'
+        out_message = f'<u>Данные не корректны!</u>\n<b> Заболевших на 03.04.2020 в Франции: 90 848</b>'
         bot.send_message(message.chat.id, out_message, parse_mode='html')
-        bot.send_message('@covid19word', out_message, parse_mode='html')
+        # bot.send_message('@covid19word', out_message, parse_mode='html')
         out_message = ''
     elif get_message_bot == 'иран':
         location = covid_19.getLocationByCountryCode('IR')
@@ -81,16 +82,16 @@ def mess(message):
         location = covid_19.getLocationByCountryCode('JP')
     elif get_message_bot == 'англия':
         location = covid_19.getLocationByCountryCode('GB')
-        out_message = f'<u>Данные не корректны!</u>\n<b> Заболевших на 03.04.2020 в Англии: 34 192</b>'
+        out_message = f'<u>Данные не корректны!</u>\n<b> Заболевших на 03.04.2020 в Англии: 42 479</b>'
         bot.send_message(message.chat.id, out_message, parse_mode='html')
-        bot.send_message('@covid19word', out_message, parse_mode='html')
+        # bot.send_message('@covid19word', out_message, parse_mode='html')
         out_message = ''
     elif get_message_bot == 'турция':
         location = covid_19.getLocationByCountryCode('TR')
     else:
         location = covid_19.getLatest()
         out_message = f'<u>Данные по всему миру:</u>\n<b> Заболевшие: </b>{location["confirmed"]}'
-        log.info(f'Called bot.. name: {message.from_user.first_name}')
+        log.info(f'Called bot.. name: {message.from_user.first_name}, not_command(message):{get_message_bot}')
 
 
     if out_message == '':
@@ -99,25 +100,25 @@ def mess(message):
         country_population = location[0]["country_population"]
         people_confirmed = location[0]["latest"]["confirmed"]
         percentage_patients_country = (int(people_confirmed) * 100) / int(country_population)
-        out_message = f'Страна - <b>{get_message_bot}</b>\n' \
-                      f'Время запуска проверки - <b>{datetime.now()}</b>\n' \
-                      f'Статистика, обновлено (вчера)* в <b>{time[0]}</b> UTC -5\n' \
-                      f'Население страны - {location[0]["country_population"]:,}\n' \
-                      f'Потверждены всего - {location[0]["latest"]["confirmed"]} *\n' \
-                      f'Погибли - {location[0]["latest"]["deaths"]} *\n' \
-                      f'Процент заболевших в стране - {percentage_patients_country:.7f} % *'
+        out_message = f'<b>Страна - {get_message_bot}</b>\n' \
+                      f'<b>Время запуска проверки - {datetime.now()}</b>\n' \
+                      f'<b>Статистика, обновлено (вчера)* в {time[0]} UTC -5</b><br>\n' \
+                      f'<b>Население страны - {location[0]["country_population"]:,}</b>\n' \
+                      f'<b>Потверждены всего - {location[0]["latest"]["confirmed"]} *</b>\n' \
+                      f'<b>Погибли - {location[0]["latest"]["deaths"]} * </b><br>\n' \
+                      f'<b>Процент заболевших в стране - {percentage_patients_country:.7f} % *<b>'
 
         print(f'Name: {message.from_user.first_name}, Date: {datetime.now()}')
         log.info(f'Called bot.. name: {message.from_user.first_name}, command: {get_message_bot}')
 
 
     bot.send_message(message.chat.id, out_message, parse_mode='html')
-    bot.send_message('@covid19word', out_message, parse_mode='html')
+    # bot.send_message('@covid19word', out_message, parse_mode='html')
 
 # счетчик
 # count_exp = 0
 
-bot.polling(none_stop=False)
+bot.polling(none_stop=True)
 
 # while True:
 #     try:
